54a0b77f3da77ec606a1c83be5d3df450eae7003
studxxx <stud181177@gmail.com>
2018-08-14 07:24:28 +0300

    fix($price) Added price product

--- src/price/migrations/m151201_170001_create_table_price_products.php
+++ src/price/migrations/m151201_170001_create_table_price_products.php
@@ -11,6 +11,8 @@ class m151201_170001_create_table_price_products extends CDbMigration
     {
         $this->createTable(self::TABLE_SCHEMA, [
             'id' => 'pk',
+            'variant_id' => 'int(11) not null',
+            'currency_id' => 'int(11) not null',
             'supplier_id' => 'int(11) not null',
             'sku' => 'string default null',
             'search' => 'string default null',
@@ -18,21 +20,20 @@ class m151201_170001_create_table_price_products extends CDbMigration
             'name' => 'string default null',
             'description' => 'string default null',
             'price' => 'string default null',
+            'final_price' => 'string default null',
             'exist' => 'string default null',
             'count' => 'string default null',
             'type' => 'string default null',
             'delivery' => 'string default null',
-            'vendor' => 'string default null',
             'note' => 'string default null',
-            'construction_number' => 'string default null',
             'constructions' => 'string default null',
             'cross_numbers' => 'string default null',
             'tecdoc_article_id' => 'string default null',
             'tecdoc_article_nr' => 'string default null',
-            'tecdoc_supplier_brand' => 'string default null',
             'tecdoc_supplier_id' => 'string default null',
+            'tecdoc_supplier_brand' => 'string default null',
             'state' => 'string default null',
-            'marker' => 'string default null', // row hash from price
+            'token' => 'string default null', // row hash from price
             'visible' => 'tinyint(1) default null', // show|hide
             'status' => 'tinyint(1) default null',
             'created_at' => 'int(11) not null',
