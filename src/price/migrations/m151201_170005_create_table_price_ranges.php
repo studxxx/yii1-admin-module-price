fecc31b2de645d87332c2e32a2339dc32e5852d2
studxxx <stud181177@gmail.com>
2018-08-14 07:42:46 +0300

    fix($price) Added price range model

--- src/price/migrations/m151201_170005_create_table_price_ranges.php
+++ src/price/migrations/m151201_170005_create_table_price_ranges.php
@@ -10,7 +10,7 @@ class m151201_170005_create_table_price_ranges extends CDbMigration
         $this->createTable(self::TABLE_SCHEMA, [
             'id' => 'pk',
             'supplier_id' => 'int(11) not null',
-            'from' => 'int(11) default null',
+            'from' => 'int(11) default 0',
             'to' => 'int(11) default null',
             'created_at' => 'int(11) not null',
             'updated_at' => 'int(11) not null',
