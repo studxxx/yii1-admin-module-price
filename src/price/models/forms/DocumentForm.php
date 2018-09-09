f7715bcd3055040c33d3d1acc75b7cde42da4132
studxxx <stud181177@gmail.com>
2018-09-09 08:16:36 +0300

    fix ($price) Added model filters, fixed add range to final price

--- src/price/models/forms/DocumentForm.php
+++ src/price/models/forms/DocumentForm.php
@@ -13,9 +13,22 @@ class DocumentForm extends DynamicForm
     {
         foreach ($templates as $template) {
             $this->defineAttribute($template->field_name);
+            if ($template->field_name === 'count') {
+                $this->addRule('count', 'filter', ['filter' => ['DocumentForm', 'filterOnlyNumber']]);
+            }
             $this->addRule($template->field_name, $template->validator);
         }
 
         parent::__construct($attributes, $scenario);
+    }
+
+    public function filterOnlyNumber($value)
+    {
+        return preg_replace('/[^0-9\.]/', '', $value);
+    }
+
+    public function filterOnlySymbol($value)
+    {
+        return preg_replace("/[^a-zA-Z0-9]/", '', $value);
     }
 }
